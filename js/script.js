10e7260ceec5fcaddcce18a31049e365505e5dce
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-07 16:06:42 +0200

    Creazione delle 3 variabili richieste ( Nome, cognome, colore preferito), Mandate a schermo per farle richiedere allutente. Grazie a document.write e all'operazione di somma ho appunto sommato tutte le variabili con in più un 24 come richiesto da traccia. Creandomi cosi una password da suggerire all'utente e da mandare in visualizazzione nel file HTML.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+alert("Ora ti aiuteremo a creare una password poco sicura")
+
+let userName = prompt("Digita qui il tuo nome")
+
+let userSurName = prompt("Ora digita il tuo Cognome, se ce ne sono, senza spazi")
+
+let userPreferedColor = prompt("So che ti sei stancato ma ti va di dirmi il tuo Colore preferito ?")
+
+document.write(userName + userSurName + userPreferedColor + 24)
+
